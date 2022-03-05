289bf573ff42867b48abeb696d0411d38a51f5a1
Harshit Shiroiya <harshitshiroiya@gmail.com>
2022-03-05 14:27:17 -0500

    Login Page - CSS (Logo & Images on FE)

--- FrontEnd/wellness-tracking/src/containers/landing-page/landing-page.tsx
+++ FrontEnd/wellness-tracking/src/containers/landing-page/landing-page.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Login } from "../../components/login/login";
-import logo from '../../assets/fitness-dial-logo.jpeg';
+import logo from '../../assets/fitness-dial-logo.png';
+import icon1 from '../../assets/running-man.png';
 import { CreatePassword } from '../../components/create-password/create-password';
 import { Signup } from '../../components/signup/signup';
 import './landing-page.scss';
@@ -10,12 +11,15 @@ export function LandingPage() {
         <div className="wellness-tracking-landing-page">
             <div className="landing-page-container">
                 <div className="landing-page-header">
+
                     <div className="logo-container">
-                        <img src={logo} alt="logo" height="100" width="100" />
-                    </div>
+                        <img src={logo} alt="logo" height="150" width="200" />
+                    </div>   
+
                 </div>
                 <div className="landing-page-body">
                     <div className="landing-page-left-pane">
+                    <img src={icon1} alt="icon1" height="600" width="550" className="icon11" />
                     </div>
                     <div className="landing-page-right-pane">
                         <Routes>
