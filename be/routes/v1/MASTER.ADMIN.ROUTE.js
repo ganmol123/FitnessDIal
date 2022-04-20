21828e70cf4915b211a5c4f3ed578c6df6ccc859
Gupta, Anmol <anmgupta@iu.edu>
2022-04-19 23:48:16 -0400

    [FEAT] added subscription model (#8)
    * [FEAT] added get all professional data
    
    * [FEAT] subscription model
    
    Co-authored-by: Anmol <anmol2feb1999@gmail.com>

--- be/routes/v1/MASTER.ADMIN.ROUTE.js
+++ be/routes/v1/MASTER.ADMIN.ROUTE.js
@@ -5,4 +5,10 @@ const admin = require("../../controllers/MASTER.ADMIN");
 
 router.post("/createProfessional", admin.createProfessional);
 
+router.post("/createCustomer", admin.createCustomer);
+
+router.post("/createPlan/:professionalId", admin.createPlan);
+
+router.get("/plans", admin.getAllPlans);
+
 module.exports = router;
